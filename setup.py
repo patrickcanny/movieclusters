3e2420bea652f873387ed83d632549e4e67f356b
Patrick Canny <patrickcanny@ku.edu>
2018-09-30 14:19:02 -0500

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='clustering with movie data',
+    author='patrick canny',
+    license='MIT',
+)
